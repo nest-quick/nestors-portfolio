e65398da227e20e2dd3df8aba4f970cf4ec85420
nest-quick <116309299+nest-quick@users.noreply.github.com>
2025-05-13 12:45:28 -0700

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+
+const App = () => {
+  return ( 
+    <Router>
+      <Navbar />
+      <div className="container my-5">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
